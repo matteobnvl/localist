6fe62ac4834280b758a2338d7b054751197bc126
matteobnvl <matteobonneval19@gmail.com>
2023-10-04 14:28:48 +0200

    add uploading file

---
+++ migrations/Version20231004122236.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231004122236 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE shop_keeper ADD email VARCHAR(255) NULL, ADD phone VARCHAR(255) NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE shop_keeper DROP email, DROP phone');
+    }
+}
--- src/Form/ShopKeeperType.php
+++ src/Form/ShopKeeperType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\ShopKeeper;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,16 @@ class ShopKeeperType extends AbstractType
     {
         $builder
             ->add('name')
+            ->add('email')
+            ->add('phone')
             ->add('streetName')
             ->add('postalCode')
             ->add('city')
+            ->add('filename', FileType::class, [
+                'label' => 'Image de votre commerce',
+                'mapped' => false,
+                'required' => true
+            ])
         ;
     }
 
