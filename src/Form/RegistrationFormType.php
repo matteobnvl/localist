e4fbdaf0cd70b9a7e502d241a968182ec085eddf
unknown <coco40090@gmail.com>
2023-10-04 14:21:43 +0200

    Dashboard

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,19 +20,25 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email', TextType::class, [
-                'label' => 'Email',
+                'label' => 'Votre email',
                 'attr' => [
-                    'placeholder' => '',
                     'class' => 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 ]
                 
             ])
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'class' => 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                ]
+            ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+                'label' => 'Votre mot de passe',
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -55,3 +61,5 @@ class RegistrationFormType extends AbstractType
         ]);
     }
 }
+
+
